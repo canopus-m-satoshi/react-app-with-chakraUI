17da7206f793ac26c83a729649b7b9ac2b88a7d0
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-09 23:20:59 +0900

    66. ルーティングの作成 https://www.udemy.com/course/react_stepup/learn/lecture/24823700?start=15#content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import theme from "./tehme/theme";
+// 外部importファイル系
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+// ローカル作成ファイル系
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
---
+++ src/router/Router.tsx
@@ -0,0 +1,39 @@
+import { memo, VFC } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { Login } from "../components/pages/Login";
+import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      {/* ログインページ */}
+      <Route exact path="/">
+        <Login />
+      </Route>
+      {/* ホーム・管理・設定ページ */}
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
+
+      {/* 404ページ */}
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
