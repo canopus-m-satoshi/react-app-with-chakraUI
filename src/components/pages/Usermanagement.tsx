73d6f3609cf058872619452cfdddb7e535fd6c20
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-14 23:21:04 +0900

    76. ユーザー詳細モーダル画面の作成 https://www.udemy.com/course/react_stepup/learn/lecture/24823750#content

--- src/components/pages/Usermanagement.tsx
+++ src/components/pages/Usermanagement.tsx
@@ -1,15 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { memo, useEffect, VFC } from "react";
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
 
 import { UserCard } from "../organism/user/UserCard";
+import { UserDetailModal } from "../organism/user/UserDetailModal";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { getUsers, loading, users } = useAllUsers();
 
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -25,11 +36,13 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
