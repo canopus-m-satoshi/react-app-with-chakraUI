e83778422fa85732c5122b142a2064c6923922d5
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-15 22:51:03 +0900

    78. ログインユーザー情報をContextに保持してみる https://www.udemy.com/course/react_stepup/learn/lecture/24823764#content

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,31 +5,34 @@ import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      {/* ログインページ */}
-      <Route exact path="/">
-        <Login />
-      </Route>
-      {/* ホーム・管理・設定ページ */}
-      <Route
-        path="/home"
-        render={({ match: { url } }) => (
-          <Switch>
-            {homeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                <HeaderLayout>{route.children}</HeaderLayout>
-              </Route>
-            ))}
-          </Switch>
-        )}
-      ></Route>
+      <LoginUserProvider>
+        {/* ログインページ */}
+        <Route exact path="/">
+          <Login />
+        </Route>
+        {/* ホーム・管理・設定ページ */}
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {homeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout>{route.children}</HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        ></Route>
+      </LoginUserProvider>
 
       {/* 404ページ */}
       <Route path="*">
